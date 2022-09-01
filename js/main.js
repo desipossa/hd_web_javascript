a785d3c86bfe96ae4240466c398f55ca6d5fccf0
lee <sipossa@naver.com>
2022-09-01 17:12:15 +0900

    20220901

--- js/main.js
+++ js/main.js
@@ -17,6 +17,23 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
 
+    // scroll event
+    // const SEC = document.querySelectorAll('.action');
+    // const WT=window.innerHeight;
+
+    // window.addEventListener('scroll', () => {
+    //     let sct = window.scrollY;
+    //     SEC.forEach(e => {
+    //         let secTop = e.offsetTop;
+    //         let secH = e.clientHeight;
+    //         if (sct > secTop - (WT - secH) / 2 - 200) {
+    //             e.classList.add('on');
+    //         };
+    //         // sct > secTop - (WT-secH) / 2 - 200 ? e.classList.add('on') : e.classList.remove('on');
+    //     });
+    // });
+
+
 
 
     window.addEventListener('scroll', () => {
